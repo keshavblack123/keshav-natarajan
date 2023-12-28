711ff09c36eb4f49b2bc5ada45d1edfcc93ec086
keshavblack123 <keshavnatarajan@gmail.com>
2023-12-28 18:22:22 +0800

    Slug linked, fast-refresh-reload error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import Image from 'next/image'
-import { useState } from 'react';
-import { Montserrat } from 'next/font/google'
+import { useState } from 'react'
 import { performRequest } from '../../lib/datocms'
+import Link from 'next/link'
+import { Montserrat } from 'next/font/google'
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] });
+const accent_color = 'bg-accent-dark dark:bg-accent-light';
+const content_color = 'text-dark dark:text-light';
+const text_accent = 'text-accent-dark dark:text-accent-light';
+const accent_content = 'text-light dark:text-dark';
 
 const PAGE_CONTENT_QUERY = `
 query Projects {
@@ -24,83 +29,76 @@ export async function getStaticProps() {
   return { props: { projects } };
 }
 
-const ToneButton = () => {
-  const [tone, setTone] = useState("light");
-
-  const handleButtonClick = () => {
-    // Toggle between "light" and "dark" when the button is clicked
-    setTone((prevTone) => (prevTone === "light" ? "dark" : "light"));
-  };
-
-  return (
-    <div className={`flex h-28 items-start justify-center bg-accent-${tone}`}>
-      <button onClick={handleButtonClick}>Toggle Tone</button>
-    </div>
-  );
-};
 
 const ProjectPreview = (props) => {
   const { data } = props;
   const tagsString = data.tags.map((tag) => tag.tagName).join(' / ');
   const formattedDate = new Date(data.date).toLocaleDateString('en-US', { month: '2-digit', year: 'numeric' }).replace('/', ' / ');
 
-  console.log(data.date)
   return (
-    <div className={`flex flex-row items-end justify-between p-6 h-28 w-full bg-accent-dark`}>
-      <div className={`flex flex-col justify-between h-full`}>
-        <h3>{data.title}</h3>
-        <p>{tagsString}</p>
+    <Link href={`/projects/${data.slug}`}>
+      <div className={`flex flex-row items-end justify-between p-6 h-28 w-full ${accent_color}`}>
+        <div className={`flex flex-col justify-between h-full ${accent_content}`}>
+          <h3>{data.title}</h3>
+          <p>{tagsString}</p>
+        </div>
+        <p className={`${accent_content}`}>{formattedDate}</p>
       </div>
-      <p>{formattedDate}</p>
-    </div>
+    </Link>
   )
 }
 
 export default function Home({ projects }) {
   const { data } = projects;
-  // posts = data.allProjects;
-  console.log(data);
+
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <main className={`flex h-screen flex-col items-center p-5 gap-5 bg-light ${montserrat.className}`}>
+    <main className={` ${montserrat.className} ${darkMode && 'dark'}`}>
+      <div className={`flex h-screen flex-col items-center p-5 gap-5 bg-light dark:bg-dark`}>
+        <div className={`flex flex-row h-full w-full gap-5`}>
+          <div className={`flex flex-col w-48 gap-5`}>
+            <div className={`flex h-28 w-full`}>
+              <Image
+                src="/favicon.ico"
+                width={165}
+                height={100}
+                alt="logo"
+              />
+            </div>
+            <div className={`flex w-full h-full justify-center`}>
+              <p className={`flex self-center text-5xl rotate-180`} style={{ writingMode: 'vertical-rl' }}>
+                <span className={`${content_color}`}>Keshav</span>&nbsp;
+                <span className={`${text_accent}`}>Natarajan</span>
+              </p>
+            </div>
+            <div className={`flex h-28 items-start justify-center ${accent_color} ${accent_content}`}
+              onClick={toggleDarkMode}>
+              <button>Toggle Tone</button>
+            </div>
+          </div>
+          <div className={`flex flex-row w-full gap-5`}>
+            <div className={`flex flex-col w-full gap-5`}>
+              <div className={`flex items-end h-28 w-full ${text_accent}`}>
+                <h1>Projects</h1>
+              </div>
+              <div className={`flex flex-col gap-3`}>
+                {projects.map((p) => (
+                  <ProjectPreview key={p.id} data={p} />
+                ))}
+              </div>
+            </div>
+            <div className={`flex flex-col w-full gap-5`}>
+              <div className={`flex items-end h-28 w-full ${text_accent}`}>
+                <h1>About</h1>
+              </div>
+            </div>
+          </div>
 
-      <div className={`flex flex-row h-full w-full gap-5`}>
-        <div className={`flex flex-col w-48 gap-5`}>
-          <div className={`flex h-28 w-full`}>
-            <Image
-              src="/favicon.ico"
-              width={165}
-              height={100}
-              alt="logo"
-            />
-          </div>
-          <div className={`flex w-full h-full justify-center`}>
-            <p className={`flex self-center text-5xl rotate-180`} style={{ writingMode: 'vertical-rl' }}>
-              <span className='text-dark'>Keshav</span>&nbsp;
-              <span className='text-accent-dark'>Natarajan</span>
-            </p>
-          </div>
-          <div className={`flex h-28 items-start justify-center`}>
-            <ToneButton />
-          </div>
         </div>
-        <div className={`flex flex-row w-full gap-5`}>
-          <div className={`flex flex-col w-full gap-5`}>
-            <div className={`flex items-end h-28 w-full text-accent-dark`}>
-              <h1>Projects</h1>
-            </div>
-            <div className={`flex flex-col gap-3`}>
-              {projects.map((p) => (
-                <ProjectPreview key={p.id} data={p} />
-              ))}
-            </div>
-          </div>
-          <div className={`flex flex-col w-full gap-5`}>
-            <div className={`flex items-end h-28 w-full text-accent-dark`}>
-              <h1>About</h1>
-            </div>
-          </div>
-        </div>
-
       </div>
     </main>
   )
---
+++ src/pages/projects/[slug].js
@@ -0,0 +1,125 @@
+import { performRequest } from '../../../lib/datocms'
+import { useState } from 'react'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+const accent_color = 'bg-accent-dark dark:bg-accent-light';
+const content_color = 'text-dark dark:text-light';
+const text_accent = 'text-accent-dark dark:text-accent-light';
+const accent_content = 'text-light dark:text-dark';
+
+
+export default function Projects({ props }) {
+  const { projectData } = props;
+
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <main className={` ${montserrat.className} ${darkMode && 'dark'}`}>
+      <div className={`flex h-screen flex-col items-center p-5 gap-5 bg-light dark:bg-dark`}>
+        <div className={`flex flex-row h-full w-full gap-5`}>
+          <div className={`flex flex-col w-48 gap-5`}>
+            <div className={`flex h-28 w-full`}>
+              <p>Logo</p>
+            </div>
+            <div className={`flex w-full h-full justify-center`}>
+              <p className={`flex self-center text-5xl rotate-180`} style={{ writingMode: 'vertical-rl' }}>
+                <span className={`${content_color}`}>Keshav</span>&nbsp;
+                <span className={`${text_accent}`}>Natarajan</span>
+              </p>
+            </div>
+            <div className={`flex h-28 items-start justify-center ${accent_color} ${accent_content}`}
+              onClick={toggleDarkMode}>
+              <button>Toggle Tone</button>
+            </div>
+          </div>
+          <div className={`flex flex-col w-full gap-5`}>
+            <div className={`flex flex-col w-full bg-dark`}>
+              <p>Back Button, prev and next</p>
+            </div>
+            <p>Content Here</p>
+          </div>
+
+        </div>
+      </div>
+    </main>
+  )
+}
+
+const PATHS_QUERY = `
+query Projects {
+  allProjects {
+    slug
+  }
+}`;
+
+export const getStaticPaths = async () => {
+  const slugQuery = await performRequest({ query: PATHS_QUERY });
+
+  let paths = [];
+  slugQuery.data.allProjects.map((p) => paths.push(`/projects/${p.slug}`));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+const PROJECTS_QUERY = `
+query Project($slug: String) {
+  project(filter: {slug: {eq: $slug}}) {
+    title
+    date
+    tags {
+      tagName
+    }
+    link {
+      value
+    }
+    summary {
+      value
+    }
+    heroImage {
+      responsiveImage {
+        width
+        webpSrcSet
+        title
+        srcSet
+        src
+        sizes
+        height
+        bgColor
+        base64
+        aspectRatio
+        alt
+      }
+    }
+    images {
+      responsiveImage {
+        alt
+        aspectRatio
+        base64
+        bgColor
+        height
+        sizes
+        src
+        srcSet
+        title
+        webpSrcSet
+        width
+      }
+    }
+  }
+}`
+
+export const getStaticProps = async ({ params }) => {
+  const { data } = await performRequest({
+    query: PROJECTS_QUERY,
+    variables: { slug: params.slug },
+  });
+
+  return { props: { projectData: data.project } };
+};
