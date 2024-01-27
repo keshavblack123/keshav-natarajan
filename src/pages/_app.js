6c37e2acffaf09a8842e4513da03060ff26fd789
keshavblack123 <keshavnatarajan@gmail.com>
2024-01-27 17:42:10 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,6 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Keshav Natarajan</title>
       </Head>
       <Component {...pageProps} />
     </>
